80e8138aaec5888ac4fb65824f8dcdead0be10ae
IragabaHerve100 <iragabaherve92@gmail.com>
2025-10-19 15:36:24 +0100

    Add files via upload

---
+++ herve.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>log in</title>
+</head>
+<body>
+    <div class='all'>
+        <div class="sum">
+            <Button  class='bb'>log in</Button>
+        </div>
+
+     
+      <div class="form"><br><br>
+      <form action="heve.php" method='post'>
+<input type="email" placeholder='your email' id='f' name='email'><br><br>
+<input type="password" placeholder='your password' name='password'>
+<button type="submit">login</button>
+</form>
+</div>  
+
+</div>
+
+<link rel="stylesheet" href="herve.css">
+</body>
+</html>
